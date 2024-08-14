6a230cb8644bde822ce4377e7e37e245f7820b69
hieupv <hieupv2412@gmaillcom>
2024-08-14 15:57:35 +0700

    connect db

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
 
 require 'functions.php';
+require 'Database.php';
+//require 'router.php';
 
-require 'router.php';
+// connect to the database, execute a query
+
+
+$config = require 'config.php';
+
+$db = new Database($config['database']);
+
+$id = $_GET['id'];
+$query = "select * from posts where id = :id";
+$posts = $db->query($query, [':id' => $id])->fetchAll(PDO::FETCH_ASSOC);
+dd($posts);
